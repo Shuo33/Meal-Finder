72b877cd2fda2ff1828dc0b6d1bde5c6f65cc061
shuo xu <shuo.xu@2021.icam.fr>
2024-12-19 12:55:16 +0100

    JS: click to show a single meal

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function searchMeal(e) {
     async function fetchMeal() {
         const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`);
         const data = await res.json();
-        console.log(data.meals);
+        // console.log(data.meals);
         resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
 
         // display meal info to the DOM: map() helps loop through each item and join() helps to make the array into a string
@@ -54,6 +54,82 @@ function searchMeal(e) {
 
 
 
+// Fetch meal detail by it's ID
+async function getMealById(mealID) {
+    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`);
+    const data = await res.json();
+    const meal = data.meals[0];
+    // console.log(meal);
+
+    addMealToDOM(meal);
+}
+
+
+// Add meal to DOM : 
+function addMealToDOM(meal) {
+    // creat a array with measurements and ingredients
+    const ingredients = [];
+
+    for (let i = 1; i <= 20; i++){
+        // to use the value of a key (inside an object) as the key of a new object/array , we need to replace '.' with '[]' since the key of the new object/array is inside a varible
+        if (meal[`strIngredient${i}`]) {
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`); 
+        } else {
+            break;
+        }
+    }
+
+
+    single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal} "/>
+
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>Category: ${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>Area: ${meal.strArea}</p>`: ''}
+      </div>
+
+      <div class="main">
+       <p>Instruction: ${meal.strInstructions}</p>
+       <h2>Ingredients</h2>
+         <ul>
+           ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+         </ul>
+      </div>
+    </div>
+    `; 
+
+
+
+    
+
+    
+}
+
+
 // Event Listeners
 // we use 'submit' since the form has a button type submit
 submit.addEventListener('submit', searchMeal);
+
+mealsEl.addEventListener('click', e => {
+    // Filter the div with 'meal-info' in order to find it's id later: 'e.composedPath()' method returns an array of objects with all the elements the current event will propogate through; find() helps to loop through all the items and find the items that pass the test parameter
+    const mealInfo = e.composedPath().find(item => {
+        if (item.classList) {
+            return item.classList.contains('meal-info');
+        } else {
+            return false;
+        }
+    });
+
+    // console.log(mealInfo);
+
+
+    // Get the ID of the clicked item
+    if (mealInfo) {
+        const mealID = mealInfo.getAttribute('data-mealid');
+        // console.log(mealID);
+
+        getMealById(mealID);
+    }   
+});
